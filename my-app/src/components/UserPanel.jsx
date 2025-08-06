42e7e0acf326e006da2e907cec893c39dae488ba
MohammadReza <mhreza.moradi@gmail.com>
2025-08-06 11:27:24 +0330

    userPanel v1

---
+++ my-app/src/components/UserPanel.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../style/UserPanel.css"
+const UserPanel=({user,onLogout})=>{
+    return(
+        <div className="user-panel-container">
+            <h3>سلام {}user.name</h3>
+            <button className="logout-btn" onClick={onLogout}>خروج</button>
+        </div>
+    )
+
+}
+export default UserPanel;
