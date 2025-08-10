1fc72945f55b7b17a5bf997118250cf0c357b197
MohammadReza <mhreza.moradi@gmail.com>
2025-08-10 09:04:10 +0330

    final chamges

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,17 +1,42 @@
 import React from "react";
 import "../style/Navbar.css";
-const Navbar =(user,cartCount)=>{
-    return(
-        <nav className="navbar-container">
-            <span className="navbar-user">
-                {user ?`ُسلام ${user.name}` :" ورود/ثبت نام"}
-            </span>
-            <span className="navbar-title">Navbar</span>
-            <span className="navbar-cart">
-                سبد خرید {cartCount}
-            </span>
-        </nav>
-    )
+import { Link } from "react-router-dom";
 
-};
-export default Navbar;
+export default function Navbar({ cartCount, user, onLogin, onLogout, searchValue, onSearchChange }) {
+  return (
+    <nav className="navbar-container">
+
+      
+      <div className="navbar-right">
+        <Link to="/" className="navbar-logo">market</Link>
+      </div>
+
+      
+      <div className="navbar-middle">
+        <input
+          className="navbar-search"
+          type="text"
+          placeholder="جستجو در محصولات..."
+          value={searchValue}
+          onChange={(e) => onSearchChange(e.target.value)}
+        />
+      </div>
+
+      
+      <div className="navbar-left">
+        {user ? (
+          <>
+            <span className="navbar-user">سلام {user.name}</span>
+            <button className="navbar-logout-btn" onClick={onLogout}>خروج</button>
+          </>
+        ) : (
+          <Link to="/login" className="navbar-login-btn">ورود | ثبت‌نام</Link>
+        )}
+        <Link to="/cart" className="navbar-cart">
+          🛒 <span>{cartCount}</span>
+        </Link>
+      </div>
+
+    </nav>
+  );
+}
