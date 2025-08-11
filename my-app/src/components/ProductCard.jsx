33ce218898e7fb8be2e2439f1d29ca93e91c311e
MohammadReza <mhreza.moradi@gmail.com>
2025-08-11 08:23:22 +0330

    product cart compelit

--- my-app/src/components/ProductCard.jsx
+++ my-app/src/components/ProductCard.jsx
@@ -1,12 +1,13 @@
+import React from "react";
 
-const ProductCard=({product,onAddToCart})=>{
-    return(
-        <div className="product-card">
-            <img className="product-image" src={product.image} alt={product.titel}/>
-            <div className="product-title">{product.titel}</div>
-            <div className="product-price">{product.price}$</div>
-            <button className="add-to-cart-btn" onClick={()=>onAddToCart(product)}>افزودن به سبد خرید </button>
-        </div>
-    )
-};
-export default ProductCard;
+export default function ProductCard({ image, title, price, onAddToCart }) {
+  if (!image) return null;
+  return (
+    <div className="product-card">
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{price} $</p>
+      <button className="product-card-add-btn" onClick={onAddToCart}>افزودن به سبد خرید</button>
+    </div>
+  );
+}
