1982c69f50e73f2960db7c9c3615ea095e57544a
MohammadReza <mhreza.moradi@gmail.com>
2025-09-06 15:36:05 +0330

    connecting backeng to frontend

--- my-app/src/api/Products.js
+++ my-app/src/api/Products.js
@@ -1,5 +1,5 @@
 export async function fetchProducts() {
-  const resp = await fetch('https://fakestoreapi.com/products?limit=99');
-  if (!resp.ok) throw new Error('خطا در دریافت محصولات');
-  return await resp.json(); 
+    const resp = await fetch('http://localhost:8080/api/products');
+    if (!resp.ok) throw new Error('خطا در دریافت محصولات');
+    return await resp.json();
 }
