0af62edb56d75a12cd7860a6779a8f4ebfb17d92
MohammadReza <mhreza.moradi@gmail.com>
2025-09-07 10:43:10 +0330

    user repository

--- src/main/java/com/store/onlinestore/repository/UserRepository.java
+++ src/main/java/com/store/onlinestore/repository/UserRepository.java
@@ -2,9 +2,13 @@ package com.store.onlinestore.repository;
 
 import com.store.onlinestore.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
     Optional<User> findByUsername(String username);
+
 }
