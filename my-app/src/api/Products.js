abc277aa0452f931f7e5a557327e3dc60bde7d0c
MohammadReza <mhreza.moradi@gmail.com>
2025-09-07 10:40:42 +0330

    port if backend is change

--- my-app/src/api/Products.js
+++ my-app/src/api/Products.js
@@ -1,5 +1,5 @@
 export async function fetchProducts() {
-    const resp = await fetch('http://localhost:8080/api/products');
+    const resp = await fetch('http://localhost:9090/api/products');
     if (!resp.ok) throw new Error('خطا در دریافت محصولات');
     return await resp.json();
 }
