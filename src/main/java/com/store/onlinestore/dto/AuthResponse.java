3f59ca933f84a741f019331f83fd92cb47eb6262
MohammadReza <mhreza.moradi@gmail.com>
2025-09-07 10:42:53 +0330

    user response

--- src/main/java/com/store/onlinestore/dto/AuthResponse.java
+++ src/main/java/com/store/onlinestore/dto/AuthResponse.java
@@ -1,10 +1,14 @@
 package com.store.onlinestore.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthResponse {
     private String massage;
     private Long userId;
