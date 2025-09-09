a1bedd14e88be7a5011d779c86c518d81828491d
MohammadReza <mhreza.moradi@gmail.com>
2025-09-09 09:42:40 +0330

    dibug authServive

--- src/main/java/com/store/onlinestore/service/AuthService.java
+++ src/main/java/com/store/onlinestore/service/AuthService.java
@@ -2,10 +2,11 @@ package com.store.onlinestore.service;
 
 import com.store.onlinestore.dto.AuthResponse;
 import com.store.onlinestore.dto.LoginRequest;
+import com.store.onlinestore.entity.User.Role;
 import com.store.onlinestore.entity.User;
 import com.store.onlinestore.repository.UserRepository;
+import com.store.onlinestore.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -16,46 +17,51 @@ import java.util.Optional;
 public class AuthService {
 
     private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
 
     public ResponseEntity<AuthResponse> registerOrLogin(LoginRequest loginRequest) {
         Optional<User> existingUser = userRepository.findByUsername(loginRequest.getUsername());
+
         if (existingUser.isPresent()) {
             User user = existingUser.get();
-            if(!user.getPassword().equals(loginRequest.getPassword())){
+
+            if (!user.getPassword().equals(loginRequest.getPassword())) {
                 return ResponseEntity
                         .status(401)
                         .body(AuthResponse.builder()
-                                .massage("رمز عبور اشتباه است ")
-                                .build()
-                        );
-
-
+                                .massage("رمز عبور اشتباه است")
+                                .build());
             }
+
+
+            String token = jwtUtil.generateJwtToken(user.getUsername());
             return ResponseEntity.ok(
                     AuthResponse.builder()
-                        .massage("ورود موفقیت آمیز ")
-                        .userId(user.getId())
-                        .username(user.getUsername())
-
+                            .massage("ورود موفقیت‌آمیز")
+                            .userId(user.getId())
+                            .username(user.getUsername())
+                            .token(token)
                             .build()
             );
-        }else {
+
+        } else {
+
             User newUser = new User();
             newUser.setUsername(loginRequest.getUsername());
             newUser.setPassword(loginRequest.getPassword());
+            newUser.setRole(Role.USER);
 
             User savedUser = userRepository.save(newUser);
 
+            String token = jwtUtil.generateJwtToken(savedUser.getUsername());
             return ResponseEntity.ok(
                     AuthResponse.builder()
-                            .massage("ثبت نام موفقیت آمیز ")
+                            .massage("ثبت‌نام موفقیت‌آمیز")
                             .userId(savedUser.getId())
                             .username(savedUser.getUsername())
+                            .token(token)
                             .build()
             );
         }
-
-
-
     }
 }
