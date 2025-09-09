646c6e55ebbb96ef15f2d41b64711ea5db7ee8e2
MohammadReza <mhreza.moradi@gmail.com>
2025-09-09 09:40:25 +0330

    add the rol

--- src/main/java/com/store/onlinestore/entity/User.java
+++ src/main/java/com/store/onlinestore/entity/User.java
@@ -2,25 +2,31 @@ package com.store.onlinestore.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "users")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
-@Table(name = "users")
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true,nullable = false)
+    @Column(unique = true, nullable = false)
     private String username;
 
     @Column(nullable = false)
     private String password;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    public enum Role {
+        USER, ADMIN
+    }
 }
